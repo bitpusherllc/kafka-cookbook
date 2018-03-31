54abb51c22196a50233e89ee023944c1ada8158c
Michael Edwards <medwards@bitpusher.com>
2018-03-31 00:40:09 -0700

    Add client recipe; bump version to 2.2.5

---
+++ recipes/client.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: kafka
+# Recipe:: client
+#
+
+include_recipe 'java::default'
+include_recipe 'kafka::_defaults'
+include_recipe 'kafka::_setup'
+include_recipe 'kafka::_install'
